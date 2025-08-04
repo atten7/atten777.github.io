fa94f6300f4113d7f543c415e2583ab2524807e4
atten7 <atten0914@gmail.com>
2025-08-04 12:07:59 +0900

    app.py

--- ppt_web/app.py
+++ ppt_web/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
 
 app = Flask(__name__)
@@ -8,7 +8,7 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', show_apikey_input=False)
 
 @app.route('/upload', methods=['POST'])
 def upload():
@@ -20,8 +20,16 @@ def upload():
     if file and file.filename.endswith('.pptx'):
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filepath)
-        return f'업로드 완료: {file.filename}'
+        return render_template('index.html', show_apikey_input=True)
     return 'pptx 파일만 업로드 가능합니다.'
+
+app.secret_key = 'your_secret_key'
+
+@app.route('/set_apikey', methods=['POST'])
+def set_apikey():
+    apikey = request.form.get('apikey')
+    session['gemini_apikey'] = apikey
+    return render_template('index.html', show_apikey_input=False, apikey_saved=True, apikey=apikey)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5001, debug=True)
